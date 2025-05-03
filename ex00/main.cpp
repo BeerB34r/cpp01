13e9e38b4b2db3a46972a4d883ba1584db4fea03
Mats De Beer <mde-beer@student.codam.nl>
2025-05-03 16:35:49 +0200

    fixed the dumb mistake i made kek

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,8 +4,9 @@ Zombie	*newZombie(std::string name);
 void	randomChump(std::string name);
 
 int	main(void) {
-	Zombie z1 = *newZombie("Jan-Åge");
+	Zombie* z1 = newZombie("Jan-Åge");
 
-	z1.announce();
+	z1->announce();
 	randomChump("Pål-Roar");
+	delete z1;
 }
